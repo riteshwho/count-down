dbdd45fd18253e3898a7cc91e20ec5fc5e3fbe73
Ritesh <riteshsinha008@gmail.com>
2023-03-24 19:14:57 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-const endDate = "7 February 2023 12:00 AM";
+const endDate = "31 december 2023 12:00 AM";
 
-document.getElementById('end-date').innerText = endDate;
+document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input");
 
 const clock = () => {
-    const end = new Date(endDate);
-    const now = new Date()
-    const diff = (end - now)/1000; // conversion to seconds from milli seconds
-    if (diff < 0) return;
-     //Conversion to Days
-    inputs[0].value = Math.floor(diff / 3600 / 24);
-    //conversion to hours 
-    inputs[1].value = Math.floor(diff / 3600) % 24;
-    //conversion to minutes
-    inputs[2].value = Math.floor(diff / 60) % 60;
-    //conversion to seconds
-    inputs[3].value = Math.floor(diff) % 60;
-
-}
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000; // conversion to seconds from milli seconds
+  if (diff < 0) return;
+  //Conversion to Days
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  //conversion to hours
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  //conversion to minutes
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  //conversion to seconds
+  inputs[3].value = Math.floor(diff) % 60;
+};
 clock();
 /**
  * 1 day = 24 hr
@@ -25,9 +24,6 @@ clock();
  * 60 min = 3600 sec
  */
 
-setInterval(
-    () =>{
-        clock();
-    },
-    1000
-)
+setInterval(() => {
+  clock();
+}, 1000);
